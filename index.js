a5941a062426077e2e3f39ff6ba740d837c793cb
Nick Romans <nicholas.romans@gmail.com>
2018-05-01 15:18:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(recipes, soups, chickennoodle) {
+ return Object.assign({}, recipes, { [soups]: chickennoodle })
+;}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, stews, beef) {recipes[stews] = beef;
+  return recipes
+;}
+
+function deleteFromObjectByKey(recipe, soups){
+delete recipes.soups;
+return recipes
+;}
+
+function destructivelyDeleteFromObjectByKey(recipes, stews){
+return recipes
+;}
